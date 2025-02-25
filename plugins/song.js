5a30f765bec5e76906645b917155d8a3c0afe05f
DINUWH-MD <dinethnethruvan772@gmail.com>
2025-02-25 08:56:20 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -1,6 +1,6 @@
 const { cmd } = require('../command')
 const yts = require('yt-search')
-const { fetchJson } = require('../lib/functions')
+const axios = require('axios')
 
 const downlink = 'https://manul-ofc-ytdl-paid-30a8f429a0a6.herokuapp.com/download/audio?url='
 
@@ -17,24 +17,25 @@ async (conn, mek, m, { from, reply, q }) => {
 
         let videoUrl = q
 
-        // If input is not a direct YouTube link, search YouTube
+        // If input is not a YouTube link, search YouTube
         if (!q.includes('youtube.com') && !q.includes('youtu.be')) {
             const searchResults = await yts(q)
             if (!searchResults.videos.length) return reply('No results found!')
-            videoUrl = searchResults.videos[0].url
+            const data = searchResults.videos[0]
+            videoUrl = data.url
+
+            let msg = `🎵 *YT SONG DOWNLOADER* 🎵\n\n📌 *Title:* ${data.title}\n⏱ *Duration:* ${data.timestamp}\n📅 *Uploaded:* ${data.ago}\n📺 *Views:* ${data.views}\n🎤 *Channel:* ${data.author.name}\n🔗 *URL:* ${data.url}`
+
+            await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: msg }, { quoted: mek })
         }
 
         const audioUrl = `${downlink}${encodeURIComponent(videoUrl)}`
-        
-        let message = `🎵 YT SONG DOWNLOADER 🎵
 
-📎 URL: ${videoUrl}
-🎧 Downloading your song...`
+        // Download & Send Audio
+        const response = await axios.get(audioUrl, { responseType: 'arraybuffer' })
+        if (!response.data) return reply('Download failed!')
 
-        await conn.sendMessage(from, { text: message }, { quoted: mek })
-
-        // Send audio as document
-        await conn.sendMessage(from, { document: { url: audioUrl }, mimetype: "audio/mp3", fileName: "song.mp3", caption: "Here is your song!" }, { quoted: mek })
+        await conn.sendMessage(from, { audio: response.data, mimetype: "audio/mp3" }, { quoted: mek })
 
     } catch (e) {
         console.log(e)
