5598d5c5c08a3c428ccbfc4e3406734d2a1e030b
DINUWH-MD <dinethnethruvan772@gmail.com>
2025-02-26 09:29:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ FOOTER: process.env.FOOTER || "Didula MD V2 💚",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 IMAGE_LIMIT: process.env.IMAGE_LIMIT || "3",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
-SESSION_ID: process.env.SESSION_ID || "JeFlVDKZ#VXkzIWmhts4oS-Ah0kguBATgu3sCoBftmBRYwvXMgPE",
+SESSION_ID: process.env.SESSION_ID || "821XwCjA#jqMheJ7hgzYsgV-mFTOzyQUQyPkzWvBFy-Kb3rkW3hk",
 OWNER_NUMBER: process.env.SESSION_ID || "94720244981",
 PREFIX: process.env.PREFIX || ".",
 ANTI_DELETE: process.env.ANTI_DELETE || "true"
